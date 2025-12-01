5813ad3218c78d0d3cb86e2bda5d29417f1f26bd
Adam Kelly <s00252192@atu.ie>
2025-12-01 00:50:43 +0000

    Added UI for Welcome Screen, Home Screen and Add Habit Screen

---
+++ app/src/main/java/com/example/a60days/ui/home/HabitUi.kt
@@ -0,0 +1,9 @@
+package com.example.a60days.ui.home
+
+data class HabitUi(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val completedDays: Int,
+    val totalDays: Int
+)
