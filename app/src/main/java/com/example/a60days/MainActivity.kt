c2d1500595b6e3d8342b7415dfedc00acab83ee1
Adam Kelly <s00252192@atu.ie>
2025-12-01 21:31:22 +0000

    Added Habit Cards to Habit Page

--- app/src/main/java/com/example/a60days/MainActivity.kt
+++ app/src/main/java/com/example/a60days/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -25,13 +22,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SixtyDaysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    WelcomeScreen {  }
-                }
+                SixtyDaysApp()
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/a60days/ui/home/homeScreen.kt
+++ app/src/main/java/com/example/a60days/ui/home/homeScreen.kt
@@ -1,21 +1,31 @@
 package com.example.a60days.ui.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.a60days.ui.theme.SixtyDaysTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,8 +56,52 @@ fun HomeScreen(
             modifier = Modifier.padding(padding).padding(16.dp)
         ) {
             items(habits) { habit ->
-//                HabitCard(habit = habit, onClick = { onHabitClick(habit.id) })
+                HabitCard(habit = habit, onClick = { onHabitClick(habit.id) })
             }
         }
+    }
+}
+
+@Composable
+fun HabitCard(habit: HabitUi, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(habit.name, style = MaterialTheme.typography.titleMedium)
+            Text(habit.description)
+            Spacer(Modifier.height(8.dp))
+            LinearProgressIndicator(
+                progress = { habit.completedDays.toFloat() / habit.totalDays.toFloat() }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    SixtyDaysTheme {
+        HomeScreen(
+            habits = listOf(
+                HabitUi(
+                    id = 1,
+                    name = "Read a book",
+                    description = "Read for at least 15 minutes every day.",
+                    completedDays = 25,
+                    totalDays = 60
+                ),
+                HabitUi(
+                    id = 2,
+                    name = "Exercise",
+                    description = "Do a 30-minute workout session.",
+                    completedDays = 45,
+                    totalDays = 60
+                )
+            ), onAddHabit = {}, onHabitClick = {}, onSettings = {}
+        )
     }
 }
